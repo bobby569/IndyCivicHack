41dc26ecee602cfdea44c205a41429f21495970f
bobby569 <zhan2175@purdue.edu>
2017-06-17 01:37:23 -0400

    Fix bug

--- private/initialize.php
+++ private/initialize.php
@@ -25,9 +25,9 @@ $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("DOC_ROOT", $doc_root);
 
- require_once('functions.php');
+require_once('functions.php');
 require_once('database.php');
- require_once('query_functions.php');
+require_once('query_functions.php');
 // require_once('validation_functions.php');
 // require_once('auth_functions.php');
 require_once('csrf_functions.php');
