3f0ed476bb2571b50c3bf756afcf897aa41a3dff
brandon <lrbn86@gmail.com>
2017-06-17 01:06:24 -0400

    Layout of Individual Registration page

--- individual.php
+++ individual.php
@@ -11,6 +11,15 @@
 body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
 .fa-anchor,.fa-coffee {font-size:200px}
+input[type=text], input[type=time], input[type=number], textarea {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
 td {
   border: 1px solid black;
 }
@@ -22,8 +31,8 @@ td {
   <div class="w3-bar w3-red w3-card-2 w3-left-align w3-small">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-center w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <!--<a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>-->
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">About Us</a>
-    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign Up / Login</a>
+    <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">About Us</a>
+    <a href="individual.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign Up / Login</a>
     <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Donations</a>
   </div>
 
@@ -35,25 +44,54 @@ td {
   </div>
 </div>
 
-<!-- Header -->
-<!--<header class="w3-container w3-red w3-center" style="padding:128px 16px">-->
-  <!--<h1 class="w3-margin w3-jumbo">BMDConnect</h1>-->
-  <!--<p class="w3-xlarge w3-italics">Change a Life</p> -->
-  <!--<button class="w3-button w3-green w3-padding-large w3-large w3-margin-top">Get Started</button>-->
-<!--</header>-->
-
 <!-- First Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-left">
       <h1>Individual Registration</h1>
-      <form id="individual">
-          <p
-      </form>
+      <div>
+        <form id="individual">
+            <p>
+              <label>
+                Title:
+                <input autofocus required placeholder="Enter title here" type="text">
+              </label>
+            </p>
+            <p>
+              <label>
+                Summary:
+                <textarea autofocus required placeholder="Enter summary here" rows="4" cols="50"></textarea>
+              </label>
+            </p>
+            <p>
+              <label>
+                Image:
+                <input autofocus required placeholder="Enter image" type="text">
+              </label>
+            </p>
+            <p>
+              <label>
+                Location:
+                <input autofocus required placeholder="Enter location here" type="text"></label>
+            </p>
+            <p>
+              <label>
+                Time:
+                <input autofocus required placeholder="Enter time here" type="time">
+              </label>
+            </p>
+            <p>
+              <label>
+                Spots left:
+                <input autofocus required placeholder= "Enter how many spots left here" type="number">
+              </label>
+            </p>
+            <button class="w3-right">Submit</button>
+        </form>
+      </div>
     </div>
   </div>
 </div>
-
 <!-- Footer -->
 <footer class="w3-container w3-padding-64 w3-center w3-opacity">  
   <div class="w3-xlarge w3-padding-32">
