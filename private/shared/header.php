9867b89317889493a6f92c5443dd9af60e1520a2
bobby569 <zhan2175@purdue.edu>
2017-06-17 09:29:01 -0400

    reduce redandency

--- private/shared/header.php
+++ private/shared/header.php
@@ -18,19 +18,4 @@ if(!isset($page_title)) {
   </head>
   <body>
 
-  <!-- Navbar -->
-  <div class="w3-top">
-      <div class="w3-bar w3-red w3-card-2 w3-left-align w3-small">
-          <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-center w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-          <a href="../../index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Home</a>
-          <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Sign Up / Login</a>
-          <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Donations</a>
-      </div>
-
-      <!-- Navbar on small screens -->
-      <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
-          <a href="../index.php" class="w3-bar-item w3-button w3-padding-large">Home</a>
-          <a href="#" class="w3-bar-item w3-button w3-padding-large">Sign Up / Login</a>
-          <a href="#" class="w3-bar-item w3-button w3-padding-large">Donations</a>
-      </div>
-  </div>
+  <?php include('./nav.php') ?>
